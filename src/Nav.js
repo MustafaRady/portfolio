a575c7cade7843c1bbf558982a727d6385298525
Moustafa Radi <116670293+MustafaRady@users.noreply.github.com>
2023-07-30 20:11:41 +0300

    new

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import M from "./m.png"
 
 
 
@@ -11,28 +11,30 @@ import { Link } from "react-router-dom";
         const section = document.getElementById(id);
             section.scrollIntoView({behavior:"auto"})
             return ; 
-        
-    
     }
     
     return(
         <>
-        <nav className=" h-12 
+        <nav className=" h-20
+                                flex
                                 text-sm font-bold 
                                 drop-shadow-xl
-                                columns-2
                                 md:text-2xl
+                                w-full
                                 "
             style={{background:"linear-gradient(to right,rgb(245, 245, 245),rgb(53, 198, 235),rgb(245, 245, 245))"}}
             id="nav">
-            <div className="py-2 px-10">
-                Portfolio
+            <div className="container py-2 md:w-1/2 w-full">
+                <div className="flex items-center md:justify-start justify-between ">
+                <img alt="" src={M} className="h-16 w-16"></img>
+                <h1 className="ml-4 text-lg">Portfolio</h1>
+                </div>
             </div>
-            <div className="flex md:justify-center hidden md:block py-2">
-                <ul className="grid grid-cols-3  ">
-                    <Link className="mx-7 hover:scale-125 duration-300" to="/">Home</Link>
-                    <Link className="mx-7 hover:scale-125 duration-300" onClick={()=>scrollDown("projects")}>Projects</Link>
-                    <Link className="mx-7 hover:scale-125 duration-300" onClick={()=>scrollDown("skills")}>Skills</Link>
+            <div className=" container w-1/2 md:justify-end hidden md:block py-2 ">
+                <ul className="flex flex-row gap-20 justify-end">
+                    <Link className=" hover:scale-125 duration-300" to="/">Home</Link>
+                    <Link className=" hover:scale-125 duration-300" onClick={()=>scrollDown("projects")}>Projects</Link>
+                    <Link className=" hover:scale-125 duration-300" onClick={()=>scrollDown("skills")}>Skills</Link>
                 </ul>
             </div>
         </nav>
